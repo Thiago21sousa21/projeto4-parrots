d150b2539808e259a936b518dec00f270363b5ed
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-04-05 17:21:35 -0300

    carta Notion

---
+++ script.js
@@ -0,0 +1,16 @@
+let c_gerador=[];
+let div_cartas;
+console.log(c_gerador);
+
+function f_imgs(){
+    let cont = 0;
+
+}
+
+function f_gerador(){
+    let cont=0;
+    while(cont<7){
+        cont++;
+    }
+}
+console.log(c_gerador);
