b04bd501dc6ab093580b1936e2bbce39d0515876
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-04-08 10:17:51 -0300

    funcionando 1.0

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*
-let lista =document.querySelector('.c-banco');
-let carta = ` <div class="card">
-                <div class="front-face face">
-                    <img src="./midia/Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png"/>
-                </div>
-                <div class="back-face face">
-                    <img src="./midia/Arquivos Úteis - Projeto 04 - Parrot Card Game/bobrossparrot.gif"/>
-
-                </div>
-            </div>
-            `
-lista.innerHTML = carta;
-*/
-
 let chave;
 let qntCartas;
 while(chave != 0){
@@ -31,25 +16,23 @@ while(chave != 0){
 
 let qntPares = qntCartas/2;
 
-const listaImgs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif"]; 
+const listaImgs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]; 
 let listaBase=[];
     let cont = 0;
     while(cont<7){
     listaBase[cont] = ` <div onclick="clicaVira(this)" class="card">
+                            <div class="nomeCarta">${listaImgs[cont]}</div>
                             <div class="front-face face">
                                 <img src="./midia/Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png"/>
                             </div>
                             <div class="back-face face">
                                 <img src="./midia/Arquivos Úteis - Projeto 04 - Parrot Card Game/${listaImgs[cont]}"/>
-
                             </div>
                         </div>
                         `;
     cont++;
 
     }
-console.log(listaBase);
-console.log(listaImgs);
 let listaDisplay = [];
 cont = 0;
 while(cont < qntPares){
@@ -57,7 +40,15 @@ while(cont < qntPares){
     listaDisplay.push(listaBase[cont]);
     cont++;
 }
-console.log(listaDisplay);
+/*
+listaDisplay.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+*/
+
 let containter = document.querySelector('.container');
 
 cont= 0;
@@ -65,12 +56,49 @@ while(cont < listaDisplay.length) {
     containter.innerHTML += listaDisplay[cont];
     cont++;
 }
-console.log();
+
+
+let comparador = [];
+let guardaTag = [];
+
+function verificaIgualdade () {
+    if(comparador[0] === comparador[1]){
+        comparador = [];
+
+        console.log(comparador);
+    }else if(comparador[0]!== comparador[1]){
+        guardaTag[0].classList.remove('virada');
+        guardaTag[0].querySelector('.front-face').classList.remove('frontClick');
+        guardaTag[0].querySelector('.back-face').classList.remove('backClick');
+        guardaTag[1].classList.remove('virada');
+        guardaTag[1].querySelector('.front-face').classList.remove('frontClick');
+        guardaTag[1].querySelector('.back-face').classList.remove('backClick');
+
+
+
+
+        guardaTag = [];
+        comparador= [];
+        console.log(comparador);
+
+    }
+
+}
 
 function clicaVira(CartaInteira){
-    const frente = CartaInteira.querySelector('.front-face')
+    let frente = CartaInteira.querySelector('.front-face')
     frente.classList.add('frontClick');
-    const traseira = CartaInteira.querySelector('.back-face')
+    let traseira = CartaInteira.querySelector('.back-face')
     traseira.classList.add('backClick');
+    CartaInteira.classList.add('virada');
 
+    let nomeCarta = CartaInteira.querySelector('.nomeCarta').innerHTML;
+    console.log(nomeCarta);
+
+    comparador.push(nomeCarta);
+    guardaTag.push(CartaInteira);
+
+    if(comparador.length > 1){
+    setTimeout(verificaIgualdade, 1000);
+    }
 }
