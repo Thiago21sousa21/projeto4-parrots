e373e1c3abddd7a98d1b849461fcd606769a1b14
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-04-08 12:44:55 -0300

    versão 1.1

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const listaImgs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif",
 let listaBase=[];
     let cont = 0;
     while(cont<7){
-    listaBase[cont] = ` <div onclick="clicaVira(this)" class="card">
+    listaBase[cont] = ` <div onclick="clicaVira(this)" class="card funcional">
                             <div class="nomeCarta">${listaImgs[cont]}</div>
                             <div class="front-face face">
                                 <img src="./midia/Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png"/>
@@ -40,14 +40,14 @@ while(cont < qntPares){
     listaDisplay.push(listaBase[cont]);
     cont++;
 }
-/*
+
 listaDisplay.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-*/
+
 
 let containter = document.querySelector('.container');
 
@@ -57,15 +57,21 @@ while(cont < listaDisplay.length) {
     cont++;
 }
 
-
+let variavelBloqueio = document.querySelectorAll('.card');
 let comparador = [];
 let guardaTag = [];
 
 function verificaIgualdade () {
     if(comparador[0] === comparador[1]){
+        let variavelDevolve = document.querySelectorAll('.funcional');
+        console.log(variavelDevolve);
+        for(let i=0; i<variavelDevolve.length; i++){
+            variavelDevolve[i].setAttribute('onclick',"clicaVira(this)");
+        }
         comparador = [];
+        guardaTag = [];
 
-        console.log(comparador);
+
     }else if(comparador[0]!== comparador[1]){
         guardaTag[0].classList.remove('virada');
         guardaTag[0].querySelector('.front-face').classList.remove('frontClick');
@@ -74,13 +80,20 @@ function verificaIgualdade () {
         guardaTag[1].querySelector('.front-face').classList.remove('frontClick');
         guardaTag[1].querySelector('.back-face').classList.remove('backClick');
 
+        guardaTag[0].classList.add('funcional');
+        guardaTag[1].classList.add('funcional');
+
+        let variavelDevolve = document.querySelectorAll('.funcional');
+        for(let i=0; i<variavelDevolve.length; i++){
+            variavelDevolve[i].setAttribute('onclick',"clicaVira(this)");
+        }
 
 
 
         guardaTag = [];
         comparador= [];
-        console.log(comparador);
 
+        
     }
 
 }
@@ -93,12 +106,19 @@ function clicaVira(CartaInteira){
     CartaInteira.classList.add('virada');
 
     let nomeCarta = CartaInteira.querySelector('.nomeCarta').innerHTML;
-    console.log(nomeCarta);
 
+    CartaInteira.classList.remove('funcional');
+    CartaInteira.removeAttribute('onclick');
     comparador.push(nomeCarta);
     guardaTag.push(CartaInteira);
 
+
     if(comparador.length > 1){
+
+    for(let i = 0; i < listaDisplay.length; i++){
+        variavelBloqueio[i].removeAttribute('onclick');
+    }    
+
     setTimeout(verificaIgualdade, 1000);
     }
 }
